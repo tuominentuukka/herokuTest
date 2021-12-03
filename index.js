018c44ee40e00f05e8110d7a69f2336ecc23ccf6
tuukka <tao.tuominen@gmail.com>
2021-12-03 10:31:33 +0200

    add database connection

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
+
 const app = express();
+const mysql = require("mysql");
+
 var cors = require("cors");
 app.use(cors());
 
 const port = process.env.PORT || 8080;
 
-const db = [{ name: "tiina" }, { name: "jack" }];
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
 
-app.get("/names", (req, res) => {
-  res.send(db);
+var pool = mysql.createPool(config);
+app.get("/", (req, res) => {
+  pool.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
